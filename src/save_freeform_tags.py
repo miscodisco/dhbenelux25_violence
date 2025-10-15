bf5578a5f306792172e2d46a5be6832d43b123d2
miscodisco <mia.jacobsen@live.dk>
2025-10-15 14:58:44 +0200

    updating to incorporate Julia's data, and to save tags per fandom instead of the whole

--- src/save_freeform_tags.py
+++ src/save_freeform_tags.py
@@ -7,17 +7,49 @@ from util import read_jsonl, write_jsonl
 
 
 def main():
-    data = read_jsonl("fics_meta.ndjson")
-    freeform_tags = []
+    # load own data and make into a dataframe
+    data = read_jsonl("data/fics_meta.ndjson")
+    df = pd.DataFrame(data)
 
-    for fic in data:
-        freeform_clean = [tag.strip().lower() for tag in fic["freeform_tags"]]
-        freeform_tags.extend(freeform_clean)
+    # load Julia's data and preprocess
+    aglr = pd.read_csv("data/anonymized_MythFic_metadata.csv", sep=";")
+    aglr["fandom_label"] = "AGLR"
+    aglr = aglr.rename(columns = {"additional tags": "freeform"})
+    aglr = aglr[aglr['freeform'].notna()]
 
-    most_popular_tags = Counter(freeform_tags).most_common(1000)
+    # combine the data 
+    cols_for_analysis = [column for column in aglr.columns if column in df.columns]
+    aglr = aglr[cols_for_analysis]
 
-    with open("most_common_tags_1000.txt", "w") as f:
-        f.write(str(most_popular_tags))
+    full_df = pd.concat([df, aglr])
+
+    # get ready for looping
+    fandoms = ["AGLR", "HP", "PJ", "LOTR"]
+
+    # we want the top 500 tags for each fandom, bc HP is so huge 
+    for fandom in tqdm(fandoms): 
+        fandom_data = full_df.loc[full_df["fandom_label"] == fandom]
+
+        freeform_tags = []
+
+        for _, fic in fandom_data.iterrows():
+            if fandom != "AGLR":
+                freeform_clean = [tag.strip().lower() for tag in fic["freeform"]]
+            elif fandom == "AGLR":
+                try:
+                    freeform_list = fic["freeform"].split(",")
+                except:
+                    print(f"something up with this fic: {fic['freeform']}")
+                    continue
+                    
+                freeform_clean = [tag.strip().lower() for tag in freeform_list]
+                
+            freeform_tags.extend(freeform_clean)
+
+        most_popular_tags = Counter(freeform_tags).most_common(500)
+
+        with open(f"tags/{fandom}_most_common_tags_500.txt", "w") as f:
+            f.write(str(most_popular_tags))
 
 
 if __name__ == "__main__":
